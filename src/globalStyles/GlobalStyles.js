9e875acdb586f1af7e318c968846f958db9d799d
Anders Larsen <andersoelarsen@gmail.com>
2020-03-08 19:43:02 +0100

    added global styles and colors

---
+++ src/globalStyles/GlobalStyles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyles = createGlobalStyle`
+  root {
+    --bright-green: #00CF75;
+    --light-green: #008F51;
+    --medium-green: #004F2D;
+    --racing-green: #004226;
+    --dark-green: #00331D ;
+    --black: #252525;    
+  }
+`
+
+export default GlobalStyles
